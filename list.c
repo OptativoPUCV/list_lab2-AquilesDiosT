3fc433fe64312bdf45a02014c7b7ca9ea4ef120e
Replit user <replituser@example.com>
2021-03-26 06:16:26 +0000

    new try

--- list.c
+++ list.c
@@ -65,12 +65,10 @@ void * prevList(List * list) {
 }
 
 void pushFront(List * list, const void * data) {
-  /*Node *n = createNode(data);
+  Node *n = createNode(data);
   if(list->head == NULL) list->head = n;
   else n->next = n; 
-  list->tail = n;*/
-
-  if(list->head == NULL) printf("cesar gei");
+  list->tail = n;
 }
 
 void pushBack(List * list, const void * data) {
