83daa6f5f5e861400a179452bcaac1683315c052
Replit user <replituser@example.com>
2021-03-26 06:06:42 +0000

    new try

--- list.c
+++ list.c
@@ -69,7 +69,7 @@ void pushFront(List * list, const void * data) {
   if(list->head == NULL) list->head = n;
   else n->next = n; 
   list->tail = n;
-  int *x = (int*)list->head;
+  int *x = (int*)list->head->data;
   printf("aaaa%i", *x);
 }
 
