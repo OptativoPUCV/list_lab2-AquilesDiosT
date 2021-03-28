ceefbd5174780b5e97e2310fac1666fe0ca28a70
Replit user <replituser@example.com>
2021-03-28 02:06:41 +0000

    new try

--- list.c
+++ list.c
@@ -110,7 +110,7 @@ void * popCurrent(List * list) {
   
   list->tail = list->tail->prev;
   list->tail->next = NULL;
-  if(list->tail->data == (void *)14) d = (void*)18;
+  printf("%p", (void *) d);
   free(n);
   return (void *)d;
 }
